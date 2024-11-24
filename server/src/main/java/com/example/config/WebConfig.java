ecf4f3dd958c47680eddbcb4d58498ecafa59c89
Zhangjason308 <zhangjason308@gmail.com>
2024-11-24 05:07:38 -0500

    add production url

--- server/src/main/java/com/example/config/WebConfig.java
+++ server/src/main/java/com/example/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to paths starting with /api
-                .allowedOrigins("http://localhost:5173") // Frontend dev server
+                .allowedOrigins("http://localhost:5173", "https://https://proud-bush-068efce0f.5.azurestaticapps.net") // Frontend dev server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Enable credentials
