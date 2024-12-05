8cccecd8d88363eb0c9cdcad86220ed4ddc3d73f
Nervith <nitinhero@hotmail.com>
2024-12-05 17:53:48 -0500

    Revert "pushing changes"
    This reverts commit fcf6207843d5c52a2bbdda5bbf2cdff1fc755375.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,10 +4,7 @@
   import Login from './components/Login.svelte'
   import Register from './components/Register.svelte'
   import Profile from './components/Profile.svelte'
-<<<<<<< Updated upstream
   import AddPerk from './components/AddPerk.svelte'
-=======
->>>>>>> Stashed changes
 
 </script>
 
@@ -16,9 +13,5 @@
   <Route path="/login" component={Login} />
   <Route path="/register" component={Register} />
   <Route path="/profile" component={Profile} />
-<<<<<<< Updated upstream
   <Route path="/addNewPerk" component={AddPerk} />
 </Router>
-=======
-</Router>
->>>>>>> Stashed changes
