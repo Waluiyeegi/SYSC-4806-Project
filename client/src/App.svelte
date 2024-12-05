4243c3c34e4caa0094c3c159b4f3041935d31f01
Nervith <nitinhero@hotmail.com>
2024-12-05 17:55:12 -0500

    Reapply "pushing changes"
    This reverts commit 8cccecd8d88363eb0c9cdcad86220ed4ddc3d73f.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,7 +4,10 @@
   import Login from './components/Login.svelte'
   import Register from './components/Register.svelte'
   import Profile from './components/Profile.svelte'
+<<<<<<< Updated upstream
   import AddPerk from './components/AddPerk.svelte'
+=======
+>>>>>>> Stashed changes
 
 </script>
 
@@ -13,5 +16,9 @@
   <Route path="/login" component={Login} />
   <Route path="/register" component={Register} />
   <Route path="/profile" component={Profile} />
+<<<<<<< Updated upstream
   <Route path="/addNewPerk" component={AddPerk} />
 </Router>
+=======
+</Router>
+>>>>>>> Stashed changes
